0b9b7da2f84b17f0f764a6a521eaba6405741b24
YONGSUN YOON <respect5716@gmail.com>
2020-08-20 09:12:04 +0900

    update

--- utils/collect_blog.py
+++ utils/collect_blog.py
@@ -8,11 +8,17 @@ from bs4 import BeautifulSoup
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--query', default='호텔')
+parser.add_argument('--query', default='카페')
 parser.add_argument('--date_from', default='20190101')
 parser.add_argument('--date_to', default='20191231')
 parser.add_argument('--num_posts', default=1000)
 
+def load_data(data_path):
+    if os.path.isfile(data_path):
+        data = pd.read_csv(data_path)
+    else:
+        data = pd.DataFrame(columns=['title', 'url', 'text'])
+    return data
 
 def url_to_soup(url):
     req = requests.get(url)
@@ -47,39 +53,41 @@ def crawl_text(url):
         text = None
     return text
 
+def save_data(data, data_path):
+    data = data.dropna().reset_index(drop=True)
+    data.to_csv(data_path, index=False, encoding='utf-8-sig')
+    return data
 
 
 def main():
     args = parser.parse_args()
+    base_dir = os.path.join(os.path.dirname(__file__), '..')
+    data_path = os.path.join(base_dir, 'data', f'{args.query}.csv')
+    
+    data = load_data(data_path)
 
-    titles, urls, texts = [], [], []
     base_url = f'https://search.naver.com/search.naver?where=post&query={args.query}&date_from={args.date_from}&date_to={args.date_to}&date_option=8'
     start = 1
-
     while True:
         query_url = base_url + f'&start={start}'
-        post_titles, post_urls = get_post_urls(query_url)
-        post_mobile_urls = [convert_to_mobile_version(i) for i in post_urls]
+        titles, urls = get_post_urls(query_url)
         
-        post_texts = []
-        for i in post_mobile_urls:
-            post_texts.append(crawl_text(i))
-            time.sleep(5)
+        for title, url in zip(titles, urls):
+            if url not in data['url'].values:
+                mobile_url = convert_to_mobile_version(url)
+                text = crawl_text(mobile_url)
+                data = data.append({'title':title, 'url':url, 'text':text}, ignore_index=True)
+                time.sleep(5)
 
-        titles += post_titles
-        urls += post_urls
-        texts += post_texts
-
+            if len(data) == args.num_posts:
+                break
+        
         start += 10
-        if len(titles) > args.num_posts:
-            break
+        data = save_data(data, data_path)
+        print(f'Collected : {str(len(data)).zfill(4)}')
 
-    data = pd.DataFrame({'title':titles, 'url':urls, 'text':texts})
-    data = data.dropna().drop_duplicates().reset_index(drop=True).iloc[:args.num_posts]
-
-    base_dir = os.path.join(os.path.dirname(__file__), '..')
-    save_path = os.path.join(base_dir, 'data', f'{args.query}.csv')
-    data.to_csv(save_path, index=False, encoding='utf-8-sig')
+    data = save_data(data, data_path)
+    
 
 if __name__ == '__main__':
     main()
